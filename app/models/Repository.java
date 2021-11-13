559066ae8b8e2da156b479252f8b772e6a425c7c
Nazanin Ahmady <nazaninahmadyshahpourabady@gmail.com>
2021-11-13 16:28:10 -0500

    Add code for userRepoIssues and JavaDocs

--- app/models/Repository.java
+++ app/models/Repository.java
@@ -2,24 +2,40 @@ package models;
 
 import java.util.List;
 
+/**
+ * @author 
+ *
+ */
 public class Repository {
     public String name;
     public Owner owner;
     public String html_url;
     public List<String> topics;
 
+    /**
+     * @return name as string
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return owner as Owner Object
+     */
     public Owner getOwner() {
         return owner;
     }
 
+    /**
+     * @return topics List
+     */
     public List<String> getTopics() {
         return topics;
     }
 
+    /**
+     * @return html_url as String
+     */
     public String getHtmlUrl() {
         return html_url;
     }
