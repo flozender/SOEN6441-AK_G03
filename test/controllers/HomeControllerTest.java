644298863a7489bf0efff8d3d7d1ff53e729bc55
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-11-20 01:08:54 -0500

    add controller tests

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -77,6 +78,7 @@ public class HomeControllerTest extends WithApplication {
             System.out.println(e);
         }
     }
+
     /**
      * Test the repositoryProfile controller action
      * 
@@ -101,5 +103,79 @@ public class HomeControllerTest extends WithApplication {
             System.out.println(e);
         }
     }
+    
+    /**
+     * Test the getRepositoryIssues controller action
+     * 
+     * @author Tayeeb Hasan
+     */
+    @Test
+    public final void testGetRepositoryIssues() {
+        final HomeController controller = testApp.injector().instanceOf(HomeController.class);
+        Cookie cookie = Cookie.builder("GITTERIFIC", String.valueOf(Math.random())).build();
+        RequestBuilder requestBuilder = Helpers.fakeRequest().cookie(cookie);
+        Request request = requestBuilder.build();
+        CompletionStage<Result> csResult = controller.getRepositoryIssues("facebook", "jest");
+        try{
+            Result result = csResult.toCompletableFuture().get();
+            String parsedResult = Helpers.contentAsString(result);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode repoIssue = mapper.readTree(parsedResult).get(0);
+            assertThat("Optional[application/json]", is(result.contentType().toString()));
+            assertThat(repoIssue.get("url").textValue(), equalTo("https://api.github.com/repos/facebook/jest/issues/11864"));
+            assertThat(repoIssue.get("title").textValue(), equalTo("[Bug]: False negative (instanceof Float32Array)"));
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
    
+    /**
+     * Test the getRepositoryContributors controller action
+     * 
+     * @author Tayeeb Hasan
+     */
+    @Test
+    public final void testGetRepositoryContributors() {
+        final HomeController controller = testApp.injector().instanceOf(HomeController.class);
+        Cookie cookie = Cookie.builder("GITTERIFIC", String.valueOf(Math.random())).build();
+        RequestBuilder requestBuilder = Helpers.fakeRequest().cookie(cookie);
+        Request request = requestBuilder.build();
+        CompletionStage<Result> csResult = controller.getRepositoryContributors("facebook", "jest");
+        try{
+            Result result = csResult.toCompletableFuture().get();
+            String parsedResult = Helpers.contentAsString(result);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode repoIssue = mapper.readTree(parsedResult).get(0);
+            assertThat("Optional[application/json]", is(result.contentType().toString()));
+            assertThat(repoIssue.get("login").textValue(), equalTo("SimenB"));
+            assertThat(repoIssue.get("html_url").textValue(), equalTo("https://github.com/SimenB"));
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Test the getRepositoryCommits controller action
+     * 
+     * @author Tayeeb Hasan
+     */
+    @Test
+    public final void testGetRepositoryCommits() {
+        final HomeController controller = testApp.injector().instanceOf(HomeController.class);
+        Cookie cookie = Cookie.builder("GITTERIFIC", String.valueOf(Math.random())).build();
+        RequestBuilder requestBuilder = Helpers.fakeRequest().cookie(cookie);
+        Request request = requestBuilder.build();
+        CompletionStage<Result> csResult = controller.getRepositoryCommits("facebook", "jest");
+        try{
+            Result result = csResult.toCompletableFuture().get();
+            String parsedResult = Helpers.contentAsString(result);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode repoIssue = mapper.readTree(parsedResult).get(0);
+            assertThat("Optional[application/json]", is(result.contentType().toString()));
+            assertThat(repoIssue.get("sha").textValue(), equalTo("7bb400c373a6f90ba956dd25fe24ee4d4788f41e"));
+            assertThat(repoIssue.get("html_url").textValue(), equalTo("https://github.com/facebook/jest/commit/7bb400c373a6f90ba956dd25fe24ee4d4788f41e"));
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
