fd1ec2dcf92806557651894699d8b6b218e52490
Nazanin Ahmady <nazaninahmadyshahpourabady@gmail.com>
2021-11-20 19:12:17 -0500

    3 test cases added + JavaDoc

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -289,6 +289,9 @@ public class HomeController extends Controller implements WSBodyReadables, WSBod
                 tempResponse.forEach(item -> {
                     issuetitles.add(item.get("title").textValue());
                 });
+                
+                
+                
                 return ok(views.html.repoissues.render(this.repoIssuesStats(issuetitles).toString().replace("{", "").replace("}", "").replace("=", "      =      ")));
             } catch (Exception e) {
                 System.out.println("CAUGHT EXCEPTION: " + e);
@@ -330,6 +333,8 @@ public class HomeController extends Controller implements WSBodyReadables, WSBod
     	    .forEachOrdered(x -> reverseCounts.put(x.getKey(), x.getValue()));
     	
 	 	return reverseCounts;
+	 	
+	 	
     	
     }
 
