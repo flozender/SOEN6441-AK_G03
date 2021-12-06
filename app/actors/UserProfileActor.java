cd3ac245d1d181d9521f5dc002265036e742845e
Pedram Nouri <noori.pedram816@gmail.com>
2021-12-06 18:56:47 -0500

    Adding userProfile actor with websocket

--- app/actors/UserProfileActor.java
+++ app/actors/UserProfileActor.java
@@ -1,2 +1,40 @@
-package actors;public class UserProfileActor {
+package actors;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import models.Owner;
+import models.Repository;
+import play.libs.ws.WSClient;
+import services.github.GitHubApi;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static akka.pattern.Patterns.pipe;
+
+public class UserProfileActor extends AbstractActor {
+
+    private static GitHubApi ghImpl;
+    private static WSClient ws;
+
+    public static Props props(WSClient ws, GitHubApi ghImpl) {
+        return Props.create(UserProfileActor.class, ws, ghImpl);
+    }
+
+    @Inject
+    public UserProfileActor(WSClient ws, GitHubApi ghImpl) {
+        this.ws = ws;
+        this.ghImpl = ghImpl;
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(GitHubActorProtocol.UserProfile.class, search -> {
+                    CompletableFuture<Owner> reply = ghImpl.userProfile(search.username, ws);
+                    pipe(reply, getContext().dispatcher()).to(sender());
+                })
+                .build();
+    }
 }
